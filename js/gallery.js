4cded3e9b062ad80393b9b974ad6447c34cf0d66
milosdelibasic <milosdelibasic97@gmail.com>
2021-04-16 18:01:17 +0200

    optimization

--- js/gallery.js
+++
@@ -1,7 +0,0 @@
-//SECTION Gallery init
-baguetteBox.run('.gallery', {
-  preload: 3,
-  overlayBackgroundColor: 'rgba(0,0,0,0.9)',
-  async: true,
-  buttons: true,
-});
--- js/main.js
+++ js/main.js
@@ -125,3 +125,11 @@ function enableScroll() {
   window.removeEventListener('touchmove', preventDefault, wheelOpt);
   window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
 }
+
+//SECTION Gallery init
+baguetteBox.run('.gallery', {
+  preload: 3,
+  overlayBackgroundColor: 'rgba(0,0,0,0.9)',
+  async: true,
+  buttons: true,
+});
